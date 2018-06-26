93591d035ed5ee5a309647cf6c4f4fa4996d1a16
Tushar Kadam <tushar.kadam@somaiya.edu>
2018-06-26 22:08:38 +0530

    Segments Intersection and Polygon Area

---
+++ Geomtry.java
@@ -0,0 +1,38 @@
+boolean seg_intersection(pair p1,pair q1,pair p2,pair q2){
+  int o1 = orientation(p1, q1, p2);
+    int o2 = orientation(p1, q1, q2);
+    int o3 = orientation(p2, q2, p1);
+    int o4 = orientation(p2, q2, q1);
+    
+    if (o1 != o2 && o3 != o4) return true;
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+    return false; 
+}
+int orientation(pair p,pair q,pair r){
+   int val = (q.y - p.y) * (r.x - q.x) -
+              (q.x - p.x) * (r.y - q.y);
+    if (val == 0) return 0;  // colinear
+    return (val > 0)? 1: 2; 
+
+}
+boolean onSegment(pair p,pair q,pair r){
+    
+    if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) &&
+        q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y))
+       return true;
+ 
+    return false;
+}
+double polygonArea(double X[], double Y[], int n){
+      double area = 0.0;
+    int j = n - 1;
+        for (int i = 0; i < n; i++)
+        {
+            area += (X[j] + X[i]) * (Y[j] - Y[i]);
+             j = i; 
+        }
+        return Math.abs(area / 2.0);
+}
