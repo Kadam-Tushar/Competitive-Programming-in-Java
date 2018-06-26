767edd083d1ecb46e1be770d2ca2ee3a7ae95325
Tushar Kadam <tushar.kadam@somaiya.edu>
2018-06-26 20:07:18 +0530

    Update sieve.java

--- sieve.java
+++ sieve.java
@@ -3,18 +3,15 @@ ArrayList<Integer> sieve(int n){
         boolean prime[] = new boolean[n+1];
         for(int i=0;i<n;i++)
             prime[i] = true;
-         
-        for(int p = 2; p*p <=n; p++)
-        {
-            if(prime[p])
-            {
-                    for(int i = p*2; i <= n; i += p)
-                    prime[i] = false;
-            }
-        }
-         
-        
- for(int i = 2; i <= n; i++)
+    for(int p = 2; p*p <=n; p++)
+      {
+         if(prime[p])
+          {
+              for(int i = p*2; i <= n; i += p)
+               prime[i] = false;
+          }
+       }
+   for(int i = 2; i <= n; i++)
    {
       if(prime[i] == true) 
           al.add(i);
