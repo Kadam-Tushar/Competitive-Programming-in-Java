762e49b77030c916cc2cd7f1af00002849653622
Tushar Kadam <tushar.kadam@somaiya.edu>
2018-06-26 19:51:01 +0530

    Sieve for n<=10^6

---
+++ sieve.java
@@ -0,0 +1,24 @@
+ArrayList<Integer> sieve(int n){
+    ArrayList<Integer> al=new ArrayList<>();
+        boolean prime[] = new boolean[n+1];
+        for(int i=0;i<n;i++)
+            prime[i] = true;
+         
+        for(int p = 2; p*p <=n; p++)
+        {
+            if(prime[p])
+            {
+                    for(int i = p*2; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+         
+        
+ for(int i = 2; i <= n; i++)
+   {
+      if(prime[i] == true) 
+          al.add(i);
+   }
+     return al;
+        
+   }
