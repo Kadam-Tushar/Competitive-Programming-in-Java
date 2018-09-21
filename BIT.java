9f7485d7b0e123108f159cf3b7a1bfc5b2f1287f
Tushar Kadam <tushar.kadam@somaiya.edu>
2018-09-21 21:15:56 +0530

    Fenwick Tree/ BIT

---
+++ BIT.java
@@ -0,0 +1,25 @@
+long[] bit=new long[8+2];
+void rangeUpdate(int l,int r,long del){
+    //you can not use rangeUpdate and RangeQuery at same time 
+    //because both have different notions for bit[] 
+    
+    update(l,del);
+    update(r+1,-del);
+}
+void update(int pos,long del){
+    while(pos<bit.length){
+        bit[pos] += del;
+        pos += Long.lowestOneBit(pos);
+    }
+}
+long sum(int pos){
+    long sum=0;
+    while(pos>0){
+        sum = (sum + bit[pos])%mod;
+        pos -= Long.lowestOneBit(pos);
+    }
+    return sum;
+}
+long rangeSum(int l ,int r){
+    return sum(r)-sum(l-1);
+}
