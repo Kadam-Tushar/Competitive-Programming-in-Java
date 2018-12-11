b63572957970984b28b055565a1f12358ac781df
Tushar Kadam <tushar.kadam@somaiya.edu>
2018-12-12 02:28:53 +0530

    Update modExpo.java

--- modExpo.java
+++ modExpo.java
@@ -1,13 +1,13 @@
-int power(int x, int y, int p) 
+long power(long x, long y, ong p) 
 { 
-   int res = 1;      
-  x = x % p;  
-    while (y > 0) 
-    { 
-        if((y & 1)==1) 
-            res = (res * x) % p; 
-        y = y >> 1;  
-        x = (x * x) % p;  
-    } 
-    return res; 
+	long res = 1;      
+	x = x % p;  
+	while (y > 0) 
+	{ 
+		if((y & 1)==1) 
+			res = (res * x) % p; 
+		y = y >> 1;  
+		x = (x * x) % p;  
+	} 
+	return res; 
 } 
