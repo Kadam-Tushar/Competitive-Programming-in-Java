93b45b07fba60026f03d79f98cb0b3522ead1a7a
Tushar Kadam <tushar.kadam@somaiya.edu>
2020-06-23 18:35:43 +0530

    Corrected lca (0 based)

--- lca.java
+++ lca.java
@@ -15,33 +15,33 @@ class Main{
             g.get(y).add(x);
         }
               
-   
+          // This is 0 based indexing
             System.out.println(lca(4,5));
        
         
         
         
     }
-    static void init(){
-        lvl=new int[n]; par=new int[n]; dp=new int[n][32];
-        for(int i=0;i<n;++i) Arrays.fill(dp[i],-1);
-        
-         dfs(0,0);
-        Arrays.fill(dp[0],0);
-        for(int i=0;i<n;++i) dp[i][0]=par[i];
+void init(){
+    lvl=new int[n]; par=new int[n]; dp=new int[n][32];
+    for(int i=0;i<n;++i) Arrays.fill(dp[i],-1);
     
-        for(int j=1;j<n;++j) for(int i=1;i<32;++i) dp[i][j]=dp[dp[i][j-1]][j-1];
-           
-    }
-    public static int lca(int u,int v){
-     
-        
-        if(u==v) return u;
-        if(lvl[u]==lvl[v]){
+     dfs(0,0);
+    Arrays.fill(dp[0],0);
+    for(int i=0;i<n;++i) dp[i][0]=par[i];
+
+    for(int j=1;j<32;++j) for(int i=1;i<n;++i) dp[i][j]=dp[ dp[i][j-1] ][ j-1 ];
+       
+}
+int lca(int u,int v){
+ 
+    
+    if(u==v) return u;
+    if(lvl[u]==lvl[v]){
         for(int i=31;i>=0;--i){
-         
+        
             if(dp[u][i]!=dp[v][i]){
-               
+            
                 if(dp[dp[u][i]][0]==dp[dp[v][i]][0])
                 {
                     return dp[dp[u][i]][0];
@@ -51,25 +51,25 @@ class Main{
         }   
         return dp[u][0];
     }
-    
-     int max=lvl[u]>lvl[v]?u:v;
-        int min=lvl[u]<lvl[v]?u:v;
-        u=max;v=min;
-        int diff=lvl[u]-lvl[v];
-        for(int i=0;i<30;++i){
-            if(((diff>>i)&1)>=1)
-                    u=dp[u][i];
-        }
-        return lca(u,v);
+
+    int max=lvl[u]>lvl[v]?u:v;
+    int min=lvl[u]<lvl[v]?u:v;
+    u=max;v=min;
+    int diff=lvl[u]-lvl[v];
+    for(int i=0;i<30;++i){
+        if(((diff>>i)&1)>=1)
+                u=dp[u][i];
     }
-    public static void dfs(int s,int p){
-        par[s]=p;
-        ArrayList<Integer> adj=g.get(s);
-        for(int x:adj){
-            if(x==p) continue;
-            
-            lvl[x]=lvl[s]+1;
-            dfs(x,s);
-        }
+    return lca(u,v);
+}
+void dfs(int s,int p){
+    par[s]=p;
+    ArrayList<Integer> adj=g.get(s);
+    for(int x:adj){
+        if(x==p) continue;
+        
+        lvl[x]=lvl[s]+1;
+        dfs(x,s);
     }
+}    
 }
