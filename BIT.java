b47fc2a03b03ff56b42ab088285333436a15e739
Kadam-Tushar <tushar.kadam@somaiya.edu>
2020-08-06 20:22:29 +0530

    fixed BIT

--- BIT.java
+++ BIT.java
@@ -1,8 +1,13 @@
-long[] bit=new long[8+2];
+long[] bit;
 void rangeUpdate(int l,int r,long del){
     //you can not use rangeUpdate and RangeQuery at same time 
     //because both have different notions for bit[] 
-    
+    // 1 indexed 
+    int n= 5 ;
+    bit = new long[n+5];
+
+    //add elements in bit by update(arr[i],1);
+
     update(l,del);
     update(r+1,-del);
 }
@@ -15,7 +20,7 @@ void update(int pos,long del){
 long sum(int pos){
     long sum=0;
     while(pos>0){
-        sum = (sum + bit[pos])%mod;
+        sum = (sum + bit[pos]);
         pos -= Long.lowestOneBit(pos);
     }
     return sum;
