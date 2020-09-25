16e0895a2cce236be2f19464b264098a14319cef
Kadam-Tushar <tushar.kadam@somaiya.edu>
2020-09-25 21:13:42 +0530

    RMQ Seg tree Added

--- SegmentTree.java
+++ SegmentTree.java
@@ -18,6 +18,8 @@ void solve() throws Exception{
 
 
 }
+
+// Query Sum 
 void build(int v,int tl,int tr,int[]arr,int[] segt){
 	if(tl==tr){
 		segt[v]= arr[tl] ;
@@ -48,5 +50,46 @@ void update(int v,int tl ,int tr,int pos,int newval,int[] segt){
 			update(2*v + 1, tm+1,tr,pos,newval,segt) ; 
 		}
 		segt[v] = segt[2*v] + segt[2*v+1];
+	}
+}
+
+
+
+
+
+
+
+// Query Max  Segment Tree 
+void build(int v,int tl,int tr,int[]arr,int[] segt){
+	if(tl==tr){
+		segt[v]= arr[tl] ;
+	}
+	else{
+		int tm = (tl+tr)/2;
+		build(2*v,tl,tm,arr,segt);
+		build(2*v+1,tm+1,tr,arr,segt);
+		segt[v] = Math.max(segt[2*v], segt[2*v + 1 ]); 
+	}
+}
+int query_max(int v,int tl,int tr ,int l ,int r,int[] segt){
+	if(l>r) return  0;
+	if(l==tl && r==tr) return segt[v];
+	int tm = (tl+tr)/2 ; 
+	return Math.max(query_max(2*v,tl,tm,l,Math.min(tm,r),segt),query_max(2*v+1,tm+1,tr,Math.max(tm+1,l),r,segt)) ; 
+}
+void update(int v,int tl ,int tr,int pos,int newval,int[] segt){
+	int tm = (tl+tr)/2;
+	if(tl == tr){
+        segt[v] = newval ; 
+        segarr[pos] = newval;
+	}	
+	else{
+		if(pos<= tm ){
+			update(2*v, tl,tm,pos,newval,segt) ; 
+		}
+		else{
+			update(2*v + 1, tm+1,tr,pos,newval,segt) ; 
+		}
+		segt[v] = Math.max(segt[2*v] , segt[2*v+1]);
 	}
 }
